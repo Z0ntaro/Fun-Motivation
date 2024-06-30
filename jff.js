b9d4ddf33d115dfc4cc0b5607d60c7ffa0d0505e
Arghyadeep Paul <103355862+Z0ntaro@users.noreply.github.com>
2024-07-01 03:25:29 +0530

    Update jff.js

--- jff.js
+++ jff.js
@@ -1,12 +1,12 @@
 const images = [
     'images/w1.jpeg',
-    'images/w2.jpeg',
     'images/w3.jpeg',
     'images/w4.jpeg',
     'images/w5.jpeg',
     'images/w6.jpeg',
     'images/w7.jpeg',
     'images/w8.jpeg',
+    'images/w9.jpeg',
 ];
 
 let currentIndex = 0;
