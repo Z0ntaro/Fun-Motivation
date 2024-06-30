1a4c4224ca8eb3196afd31ca39e5c4cc84492662
Arghyadeep Paul <103355862+Z0ntaro@users.noreply.github.com>
2024-07-01 01:24:26 +0530

    Add files via upload

---
+++ jff.js
@@ -0,0 +1,17 @@
+const images = [
+    'images/w1.jpeg',
+    'images/w2.jpeg',
+    'images/w3.jpeg',
+    'images/w4.jpeg',
+    'images/w5.jpeg',
+    'images/w6.jpeg',
+    'images/w7.jpeg',
+    'images/w8.jpeg',
+];
+
+let currentIndex = 0;
+
+function changeImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    document.getElementById('mainImage').src = images[currentIndex];
+}
